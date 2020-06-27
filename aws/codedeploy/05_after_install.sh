a23fc8165a3c8a071735dbefd4b8834e45a77e91
MarkC65 <mark.chalinder@gmail.com>
2020-06-27 14:45:41 +0100

    Amended

--- aws/codedeploy/05_after_install.sh
+++ aws/codedeploy/05_after_install.sh
@@ -6,8 +6,10 @@
 # Point to correct DB
 # Set the env vars for the appropriate staging/test or production environment
 # Disable redis caching for non-production. Point to correct elasticache in production
-echo "AfterInstall starting..."
-cd /var/www
-bundle install --without development:test --path vendor/bundle --binstubs vendor/bundle/bin -j4 --deployment
-bundle exec rake assets:precompile RAILS_ENV=production
+echo "AfterInstall starting..." >> tmp/05.txt
+cd /var/www >> tmp/05.txt
+aws s3 cp s3://eb-rails-server/credentials.yml.enc ./config/credentials.yml.enc >> tmp/05.txt
+aws s3 cp s3://eb-rails-server/master.key ./config/master.key >> tmp/05.txt
+bundle install --without development:test --path vendor/bundle --binstubs vendor/bundle/bin -j4 --deployment >> tmp/05.txt
+bundle exec rake assets:precompile RAILS_ENV=production >> tmp/05.txt
 echo "AfterInstall complete."
--- aws/codedeploy/06_start_server.sh
+++ aws/codedeploy/06_start_server.sh
@@ -3,8 +3,8 @@
 # that were stopped during ApplicationStop.
 # Run the rails server command - same command for all environments
 echo "ApplicationStart starting..."
-cd /var/www
-export RACK_ENV=production
-export RAILS_SERVE_STATIC_FILES=true
-export PORT=3000
-bundle exec puma -t 5:5 -p ${PORT:-3000} -e ${RACK_ENV:-production} &
+cd /var/www >> tmp/06.txt
+export RACK_ENV=production >> tmp/06.txt
+export RAILS_SERVE_STATIC_FILES=true >> tmp/06.txt
+export PORT=3000 >> tmp/06.txt
+bundle exec puma -t 5:5 -p ${PORT:-3000} -e ${RACK_ENV:-production} & >> tmp/06.txt
