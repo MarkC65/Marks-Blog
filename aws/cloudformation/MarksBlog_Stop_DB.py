0f848027f6c7ae44201bff0648350bdc243c94ef
MarkC65 <mark.chalinder@gmail.com>
2020-07-03 13:11:48 +0100

    Update Lambda functions

--- aws/cloudformation/MarksBlog_Stop_DB.py
+++ aws/cloudformation/MarksBlog_Stop_DB.py
@@ -1,19 +1,63 @@
-import json
+from __future__ import print_function
+import boto3
+import traceback
 
+client = boto3.client('rds')
+code_pipeline = boto3.client('codepipeline')
+
+def put_job_success(job, message):
+    """Notify CodePipeline of a successful job
+    
+    Args:
+        job: The CodePipeline job ID
+        message: A message to be logged relating to the job status
+        
+    Raises:
+        Exception: Any exception thrown by .put_job_success_result()
+    
+    """
+    print('Putting job success')
+    print(message)
+    code_pipeline.put_job_success_result(jobId=job)
+
+def put_job_failure(job, message):
+    """Notify CodePipeline of a failed job
+    
+    Args:
+        job: The CodePipeline job ID
+        message: A message to be logged relating to the job status
+        
+    Raises:
+        Exception: Any exception thrown by .put_job_failure_result()
+    
+    """
+    print('Putting job failure')
+    print(message)
+    code_pipeline.put_job_failure_result(jobId=job, failureDetails={'message': message, 'type': 'JobFailed'})
+ 
 def lambda_handler(event, context):
-    import boto3
-
-    client = boto3.client('rds')
-
-    response = client.describe_db_instances(
-        DBInstanceIdentifier='prod',
-    )
-
-    if (response['DBInstances'][0]['DBInstanceStatus']) == "available":
-        response = client.stop_db_instance(
-            DBInstanceIdentifier='prod'
+    """The Lambda function handler
+    Args:
+        event: The event passed by Lambda
+        context: The context passed by Lambda
+    """
+    try:
+        # Extract the Job ID
+        job_id = event['CodePipeline.job']['id']
+        response = client.describe_db_instances(
+            DBInstanceIdentifier='prod',
         )
-
-        return 0
-    else:
-        return 1
+        if (response['DBInstances'][0]['DBInstanceStatus']) == "available":
+            response = client.stop_db_instance(
+                DBInstanceIdentifier='prod'
+            )
+            put_job_success(job_id, 'RDS stop_db_instance call completed.') 
+        else:
+            put_job_failure(job_id, 'RDS DB not in Available state.')
+    except Exception as e:
+        # If any other exceptions which we didn't expect are raised
+        # then fail the job and log the exception message.
+        print('Function failed due to exception.') 
+        print(e)
+        traceback.print_exc()
+        put_job_failure(job_id, 'Function exception: ' + str(e))
--- aws/cloudformation/MarksBlog_Wait_For_DB.py
+++ aws/cloudformation/MarksBlog_Wait_For_DB.py
@@ -1,16 +1,62 @@
-import json
+from __future__ import print_function
+import boto3
+import traceback
 
+client = boto3.client('rds')
+code_pipeline = boto3.client('codepipeline')
+
+def put_job_success(job, message):
+    """Notify CodePipeline of a successful job
+    
+    Args:
+        job: The CodePipeline job ID
+        message: A message to be logged relating to the job status
+        
+    Raises:
+        Exception: Any exception thrown by .put_job_success_result()
+    
+    """
+    print('Putting job success')
+    print(message)
+    code_pipeline.put_job_success_result(jobId=job)
+
+def put_job_failure(job, message):
+    """Notify CodePipeline of a failed job
+    
+    Args:
+        job: The CodePipeline job ID
+        message: A message to be logged relating to the job status
+        
+    Raises:
+        Exception: Any exception thrown by .put_job_failure_result()
+    
+    """
+    print('Putting job failure')
+    print(message)
+    code_pipeline.put_job_failure_result(jobId=job, failureDetails={'message': message, 'type': 'JobFailed'})
+ 
 def lambda_handler(event, context):
-    import boto3
-
-    client = boto3.client('rds')
-
-    waiter = client.get_waiter('db_instance_available')
-
-    waiter.wait(
-        DBInstanceIdentifier='prod',
-        WaiterConfig={
-            'Delay': 15,
-            'MaxAttempts': 40
-        }
-    )
+    """The Lambda function handler
+    Args:
+        event: The event passed by Lambda
+        context: The context passed by Lambda
+    """
+    try:
+        # Extract the Job ID
+        job_id = event['CodePipeline.job']['id']
+        waiter = client.get_waiter('db_instance_available')
+        waiter.wait(
+            DBInstanceIdentifier='prod',
+            WaiterConfig={
+                'Delay': 15,
+                'MaxAttempts': 40
+            }
+        )
+        put_job_success(job_id, 'RDS db_instance_available get_waiter call completed.') 
+    except Exception as e:
+        # If any other exceptions which we didn't expect are raised
+        # then fail the job and log the exception message.
+        print('Function failed due to exception.') 
+        print(e)
+        traceback.print_exc()
+        put_job_failure(job_id, 'Function exception: ' + str(e))
